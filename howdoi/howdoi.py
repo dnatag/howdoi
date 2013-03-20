59bbeb97f8cc34a33f5f68a40205e8a329c69992
dnatag <dnatag@gmail.com>
2013-03-20 14:24:07 -0300

    Fix unicode format.
    Fix the crash due to unicode decode error in the following search:
    howdoi.py -p2 python unittest

--- howdoi/howdoi.py
+++ howdoi/howdoi.py
@@ -27,7 +27,7 @@ from requests.exceptions import ConnectionError
 
 SEARCH_URL = 'https://www.google.com/search?q=site:stackoverflow.com%20{0}'
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17'
-ANSWER_HEADER = '--- Answer {0} ---\n{1}'
+ANSWER_HEADER = u'--- Answer {0} ---\n{1}'
 NO_ANSWER_MSG = '< no answer given >'
 
 
